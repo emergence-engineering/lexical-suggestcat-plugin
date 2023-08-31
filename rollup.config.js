9406257a31f7202678028f7161e3e257f1f9a055
Istvan Matejcsok <119620946+matejcsok-ee@users.noreply.github.com>
2023-08-31 20:14:36 +0200

    config: 🔧 add rollup, eslint, scripts

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
+import css from "rollup-plugin-import-css";
+import babel from "rollup-plugin-babel";
+import excludeDependenciesFromBundle from "rollup-plugin-exclude-dependencies-from-bundle";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    { file: pkg.module, format: "es" },
+  ],
+  external: [...Object.keys(pkg.dependencies || {})],
+  plugins: [
+    excludeDependenciesFromBundle(),
+    resolve({
+      extensions: [".js", ".jsx", ".ts", ".tsx"], // Include JSX file extension
+    }),
+    commonjs(),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    }),
+    copy({
+      targets: [{ src: "src/styles/*", dest: "dist/styles" }],
+    }),
+    css(),
+    typescript(),
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const init = () => {
+  console.log("lexical-suggestcat-plugin");
+};
